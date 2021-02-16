b97535781bf2aafaed563e3a879175ddfdc9f524
Jesus Rodriguez Valencia <jesus.rodriguez.jr3@roche.com>
2021-02-16 14:21:29 +0100

    initial unit testing

---
+++ WebApp/WebApp.UnitTests/WeatherForecastControllerUnitTests.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WebApp.Controllers;
+
+namespace WebApp.UnitTests
+{
+    [TestClass]
+    public class WeatherForecastControllerUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           //Arrange
+           var loggerMock = new Mock<ILogger<WeatherForecastController>>();
+           var weatherForecastController = new WeatherForecastController(loggerMock.Object);
+
+
+            //act
+            var result = weatherForecastController.Get();
+
+            //Assert
+            Assert.IsTrue(result.Count() == 5);
+        }
+    }
+}
